d5c1cfdd89bba72fc329ce04b2234e62ddfa6087
Austin Horstman <khaneliman12@gmail.com>
2024-03-25 23:15:25 -0500

    chore: more performance tweaks

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,7 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use std::process::Command;
-use tokio::{io::AsyncBufReadExt, net::UnixStream};
+use tokio::{io::AsyncBufReadExt, net::UnixStream, process::Command};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -79,42 +78,44 @@ async fn handle_event(line: &str, config: &Config) -> Result<(), Box<dyn std::er
                     let command_str = format!("{},{}", config.monitor, wallpaper.display());
 
                     debug!("Command: hyprctl hyprpaper wallpaper {}", &command_str);
+                    tokio::spawn(async move {
+                        let output = Command::new("hyprctl")
+                            .args(["hyprpaper", "wallpaper", &command_str])
+                            .output()
+                            .await;
 
-                    let output = Command::new("hyprctl")
-                        .args(["hyprpaper", "wallpaper", &command_str])
-                        .output();
+                        match output {
+                            Ok(output) => {
+                                debug!("Command output: {:?}", output);
 
-                    match output {
-                        Ok(output) => {
-                            debug!("Command output: {:?}", output);
+                                match output.status.success() {
+                                    true => {
+                                        let result = String::from_utf8(output.stdout)
+                                            .expect("Invalid UTF-8 sequence");
 
-                            match output.status.success() {
-                                true => {
-                                    let result = String::from_utf8(output.stdout)
-                                        .expect("Invalid UTF-8 sequence");
-
-                                    if result.contains("wallpaper failed (not preloaded)")
-                                        || result.contains(&format!(
-                                            "Couldn't connect to /tmp/hypr/{}/.hyprpaper.sock",
-                                            env::var("HYPRLAND_INSTANCE_SIGNATURE")?
-                                        ))
-                                    {
-                                        error!("Wallpaper setting failed: {}", result);
-                                    } else {
-                                        debug!("Command output: {}", result);
+                                        if result.contains("wallpaper failed (not preloaded)")
+                                            || result.contains(&format!(
+                                                "Couldn't connect to /tmp/hypr/{:?}/.hyprpaper.sock",
+                                                env::var("HYPRLAND_INSTANCE_SIGNATURE")
+                                            ))
+                                        {
+                                            error!("Wallpaper setting failed: {}", result);
+                                        } else {
+                                            debug!("Command output: {}", result);
+                                        }
+                                    }
+                                    false => {
+                                        let error = String::from_utf8(output.stderr)
+                                            .expect("Invalid UTF-8 sequence");
+                                        error!("Error executing command: {}", error);
                                     }
                                 }
-                                false => {
-                                    let error = String::from_utf8(output.stderr)
-                                        .expect("Invalid UTF-8 sequence");
-                                    error!("Error executing command: {}", error);
-                                }
+                            }
+                            Err(error) => {
+                                error!("Error executing command: {}", error);
                             }
                         }
-                        Err(error) => {
-                            error!("Error executing command: {}", error);
-                        }
-                    }
+                    });
                 }
                 Err(error) => {
                     error!("Error getting wallpaper: {}", error);
